40a31d63991ca94257a8090a34ca0fa1e3a1e9c7
Cam Nguyen Tran <73975520+camnguyenmeow@users.noreply.github.com>
2022-08-10 16:39:57 +0700

    Add files via upload

---
+++ js/homepage.js
@@ -0,0 +1,30 @@
+function abtme(open) {
+	var abdiv = document.getElementById("about"),
+	maindiv = document.getElementById("maindiv");
+	if (open) {
+		abdiv.style.display = 'block';
+		maindiv.style.display = 'none';
+	} else {
+		abdiv.style.display = 'none';
+		maindiv.style.display = 'block';
+	}
+}
+function htp(open) {
+	var abdiv = document.getElementById("htp"),
+	maindiv = document.getElementById("maindiv");
+	if (open) {
+		abdiv.style.display = 'block';
+		maindiv.style.display = 'none';
+	} else {
+		abdiv.style.display = 'none';
+		maindiv.style.display = 'block';
+	}
+}
+function playMusic() {
+	const musicFile = 'music/background.mp3';
+	const audio = new Audio(musicFile);
+	audio.volume = 0.35;
+	audio.loop = true;
+	audio.play();
+}
+document.addEventListener('click', playMusic, {once: true})
